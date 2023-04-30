65bfb6a66e8f83fe60bff0e91d256514cc4dc36e
Huangdi-599 <92282845+Huangdi-599@users.noreply.github.com>
2023-04-30 23:29:42 +0100

    task 0 to 12

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,33 +1,36 @@
 #!/usr/bin/python3
 
-'''
+"""
 Contains a Flask web application API.
-'''
+"""
 
 import os
 from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 
 
 app = Flask(__name__)
-'''The Flask web application instance.'''
+"""The Flask web application instance."""
 app.register_blueprint(app_views)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
 def close_storage(exception):
-    '''The Flask app/request context end event listener.'''
+    """The Flask app/request context end event listener."""
     storage.close()
 
 
 @app.errorhandler(404)
 def not_found(error):
-    '''Handles the 404 HTTP error code.'''
+    """Handles the 404 HTTP error code."""
     return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == '__main__':
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = os.getenv('HBNB_API_PORT', 5000)
+
     app.run(host=host, port=port, threaded=True)
