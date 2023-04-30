e7556be6da8c7275970bcad28284b657acffeccd
Huangdi-599 <92282845+Huangdi-599@users.noreply.github.com>
2023-04-30 23:08:46 +0100

    fixed

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -8,24 +8,33 @@ from api.v1.views import app_views
 from models import storage
 from models.state import State
 
+
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def get_all_states():
-    """Retrieves the list of all State objects"""
+    """
+    Retrieves the list of all State objects.
+    """
     states = storage.all(State).values()
-    states_list = [state.to_dict() for state in states]
-    return jsonify(states_list)
+    states_dict = [state.to_dict() for state in states]
+    return jsonify(states_dict)
+
 
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
 def get_state(state_id):
-    """Retrieves a State object"""
+    """
+    Retrieves a State object.
+    """
     state = storage.get(State, state_id)
     if state is None:
         abort(404)
     return jsonify(state.to_dict())
 
+
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def create_state():
-    """Creates a State object"""
+    """
+    Creates a State object.
+    """
     if not request.json:
         abort(400, "Not a JSON")
     if 'name' not in request.json:
@@ -35,9 +44,12 @@ def create_state():
     state.save()
     return jsonify(state.to_dict()), 201
 
+
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
 def update_state(state_id):
-    """Updates a State object"""
+    """
+    Updates a State object.
+    """
     state = storage.get(State, state_id)
     if state is None:
         abort(404)
@@ -50,9 +62,12 @@ def update_state(state_id):
     state.save()
     return jsonify(state.to_dict())
 
+
 @app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
 def delete_state(state_id):
-    """Deletes a State object"""
+    """
+    Deletes a State object.
+    """
     state = storage.get(State, state_id)
     if state is None:
         abort(404)
